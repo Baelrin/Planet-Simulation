84400bd5077f9b7210bb45f9588635c2164f7103
Baelrin <Baelrin-Revel@yandex.ru>
2024-01-10 16:44:41 +0300

    Create basic structure - Add quit option

--- master.py
+++ master.py
@@ -1,1 +1,18 @@
+import pygame
+import math
+pygame.init()
 
+WIDTH, HEIGHT = 800, 800
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Planets')
+
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
